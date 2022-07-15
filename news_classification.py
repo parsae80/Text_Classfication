0f5623e5b86f3d26458080883aee4c37c8f94f39
parsae80 <67233772+parsae80@users.noreply.github.com>
2022-07-15 11:52:01 +0430

    Update news_classification.py

--- news_classification.py
+++ news_classification.py
@@ -1,5 +1,6 @@
 import csv
-
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 stopwords = []
 with open('PersianStopWords.txt', 'r', encoding='utf-8')as stopWordsList:
@@ -22,18 +23,16 @@ news_did = []
 news_body = ""
 is_first_did = True
 with open('Hamshahri-Sample.txt', 'r', encoding='utf-8') as hCorpus:
-    count = 0
     for line in hCorpus:
         line = line.replace('\t', ',')
         new_line = ""
-        if line.strip().startswith(".DID") or is_first_did:
+        if line.strip().startswith(".DID") or is_first_did or not line:
             if is_first_did:
                 is_first_did = False
-            count += 1
-            if count == 2:
-                full_body_news.append(news_body+"")
-                news_body = ""
-                count = 0
+
+            if news_body != '':
+                full_body_news.append(news_body)
+            news_body = ""
             new_line = line.replace('\t', ',')
             new_line = new_line.replace('\n', '')
             new_line = new_line.split(',')
@@ -50,10 +49,13 @@ with open('Hamshahri-Sample.txt', 'r', encoding='utf-8') as hCorpus:
             new_line = new_line.split(',')
             news_date.append(new_line[1])
         else:
-            news_body += line.strip()
-            for word in news_body.split(' '):
+            news_body_temp = ''
+            news_body_temp += line.strip()
+            for word in news_body_temp.split(' '):
                 if word in stopwords:
-                    news_body = news_body.replace(word, '')
+                    news_body_temp = news_body_temp.replace(word, '')
+
+            news_body += news_body_temp
 
             news_cat_body_list = list(zip(news_category, full_body_news))
 
@@ -65,3 +67,10 @@ with open('Hamshahri_Corpus.csv', 'w', encoding='utf-8')as hCorpusCSV:
     writer.writerow(news_cat_body_list)
 
 
+#
+# features = pd.read_csv('Hamshahri_Corpus.csv')
+# # One-hot encode the data using pandas get_dummies
+# features = pd.get_dummies(features)
+# train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+# # print(test)
+# print(train)
